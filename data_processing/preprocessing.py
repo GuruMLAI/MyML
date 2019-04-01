b39196ef522cb51007139661612e0d513dfc75db
Guruprasad <guru241987@gmail.com>
2019-04-01 23:11:59 +0530

    Comments

--- data_processing/preprocessing.py
+++ data_processing/preprocessing.py
@@ -7,6 +7,14 @@ import pandas as pd
 from itertools import combinations
 
 class DataLoader:
+    '''
+    This can be used for loading the data.
+    The user has to choose the folder where the training and the test dat aare located.
+    Also specify the names of the files. The default file format is assumed to be csv.
+
+    The __init__ method defines the data directory and the training and test files.
+    The load method does the loading of the data into train and test datasets.
+    '''
 
     def __init__(self, train='train.csv', test='test.csv'):
 
@@ -23,6 +31,14 @@ class DataLoader:
 
 
 class InteractionDefiner:
+    '''
+    This class defines the Interactions in the dataset between a defined set of input variables.
+    The __init__ method defines the iterator which generates all the possible combinations of the variables
+    that have been defined in features
+
+    The type of interactions that are intended to be explored (e.g 2-way, 3-way etc.
+     can be defined in types as a list
+    '''
 
     def __init__(self, features, types):
 
