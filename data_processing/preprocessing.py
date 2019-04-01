c895c633556b0bf53006bb06c349b5b940327c80
Guruprasad <guru241987@gmail.com>
2019-04-01 06:31:42 +0530

    Added Interaction Indentifier Class

--- data_processing/preprocessing.py
+++ data_processing/preprocessing.py
@@ -2,11 +2,14 @@ import os
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
 
+import numpy as np
 import pandas as pd
+from itertools import combinations
 
 class DataLoader:
 
     def __init__(self, train='train.csv', test='test.csv'):
+
         print('Please select the directory that has the data.')
         Tk().withdraw()
         self.data_path = askdirectory()
@@ -17,4 +20,31 @@ class DataLoader:
     def load(self):
 
         return pd.read_csv(self.train_data), pd.read_csv(self.test_data)
+
+
+class InteractionDefiner:
+
+    def __init__(self, features, types):
+
+        self.features = features
+        self.types = types
+
+        self.iterator = []
+        for i in self.types:
+            for x in combinations(np.arange(len(self.features)),i):
+                self.iterator.append(x)
+
+        print(self.iterator)
+
+
+    def calculate(self, data):
+
+        for i in self.iterator:
+
+            f_list = [self.features[k] for k in list(i)]
+            f_name = '_'.join(f_list)
+            data[f_name] = data[f_list].prod(axis=1)
+
+        return data
+
 
--- ml_build.py
+++ ml_build.py
@@ -1,7 +1,12 @@
-from data_processing.preprocessing import DataLoader
+from data_processing.preprocessing import DataLoader, InteractionDefiner
 
+# Load the data
 dl = DataLoader()
 train, test = dl.load()
+print(train)
 
+# Interaction Definer
+Id = InteractionDefiner(['Pclass','SibSp','Parch','Survived'],[2,3])
+print(Id.calculate(train))
 
 
