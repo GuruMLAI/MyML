62ededbd8ba237f7977a57e0f5f21fee7ff20156
Guruprasad <guru241987@gmail.com>
2019-04-06 07:28:38 +0530

    Completed cross-validation

--- data_processing/utils.py
+++ data_processing/utils.py
@@ -1,17 +1,41 @@
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, accuracy_score
 from sklearn.model_selection import KFold
 
-def cross_validation_score( data, features, label, fold):
+
+def metric_calc(model, X, y, metric='auc'):
+    if metric == 'auc':
+        _metric = roc_auc_score(y, model.predict_proba(X)[:, 1])
+    else:
+        _metric = accuracy_score(y, [round(value) for value in model.predict_proba(X)[:, 1]])
+
+    return _metric
+
+def cross_validation_score(model, data, features, label, metric='auc', fold=5):
 
     kf = KFold(n_splits=fold, shuffle=True)
     kf.get_n_splits(data)
 
+    mean_metric = 0
     for train_index, test_index in kf.split(data):
 
         X_train, X_test = data.iloc[train_index][features], data.iloc[test_index][features]
         y_train, y_test = data.iloc[train_index][label], data.iloc[test_index][label]
 
+        ml = model()
+        ml.fit(X_train, y_train)
+        #print(y_test)
+        #print(ml.predict_proba(X_test))
+        #ml_metric = roc_auc_score(y_test, ml.predict_proba(X_test))
+        ml_metric = metric_calc(ml, X_test, y_test, metric)
 
-    print('Done.')
+        mean_metric += ml_metric/fold
+
+    print('The mean cross-validation {} score is :{}'.format(metric, mean_metric))
+
+    full_ml = model()
+    full_ml.fit(data[features], data[label])
+    full_metric = metric_calc(full_ml, data[features], data[label], metric)
+
+    return full_ml, mean_metric
 
 
--- ml_build.py
+++ ml_build.py
@@ -1,13 +1,14 @@
 from data_processing.preprocessing import DataLoader, InteractionDefiner
 
 from data_processing.utils import cross_validation_score
+from sklearn.linear_model import LogisticRegression
 
 # Load the data
 dl = DataLoader()
 train, test = dl.load()
 #print(train)
 
-cross_validation_score(train,['Pclass','SibSp','Parch'],'Survived', 3 )
+model, metric = cross_validation_score(LogisticRegression, train,['Pclass','SibSp','Parch'],'Survived', 'auc', 3 )
 
 # Interaction Definer
 #Id = InteractionDefiner(['Pclass','SibSp','Parch','Survived'],[2,3])
