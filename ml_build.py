fbb9194f608b3125b27b9330edce80a22bcb4b83
Guruprasad <guru241987@gmail.com>
2019-04-10 15:04:11 +0200

    Added the run_params dictionary for parameterization of the runs

--- ml_build.py
+++ ml_build.py
@@ -16,6 +16,7 @@ run_params = {
 dl = DataLoader()
 train, test = dl.load()
 
+
 # Encoder
 ec = Encoder(run_params.get('encode_variables'))
 ec.find_values(train)
@@ -23,13 +24,13 @@ ec.find_values(train)
 train = ec.encode(train)
 test = ec.encode(test)
 
+
 # Interaction Definer
 interaction_variables = [col for col in train.columns if col not in run_params.get('ID_variables')+run_params.get('label')]
 Id = InteractionDefiner(interaction_variables,[2,3])
 train, test = Id.calculate(train), Id.calculate(test)
-print(train.columns)
 
-'''
+
 # Feature Selection
 fs = FeatureSelection()
 
@@ -37,6 +38,4 @@ features = list(train.columns)
 features = [col for col in features if col not in ['Survived','Sex','Embarked']]
 min_th_feat = fs.first_criteria(train, features, run_params.get('label'), 0.6)
 print(min_th_feat)
-
-'''
 
